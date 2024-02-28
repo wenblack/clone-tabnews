7b96a5b35b0ba3c0cd843ff3f7a4ff6db7630c2a
Wender Barbosa <wender.black@hotmail.com>
2024-02-27 22:24:50 -0300

    Create custom message and publish(1st Challenge)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Hello World!</h1>
+  return <h1 style={{
+    display: 'flex',
+    height: '100vh',
+    justifyContent: "center",
+    alignItems: "center",
+    marginInline: '5%'
+  }}>Dessa vez eu vou até o final deste curso! E obrigado por não desistir de mim ❤️ 😊</h1>
 }
 
 export default Home
